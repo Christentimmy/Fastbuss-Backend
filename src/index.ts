54689b13c10acc1586990343bffa4d9f3980662a
Christentimmy <timmychris09@gmail.com>
2025-05-28 15:26:15 -0700

    cors fixed

--- src/index.ts
+++ src/index.ts
@@ -25,20 +25,22 @@ const port = process.env.PORT || 5000;
 
 
 app.use(helmet());
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     const allowedOrigins = [
-//       process.env.LOCAL,
-//       process.env.ORIGIN,
-//     ];
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-//   credentials: true
-// }));
+app.use(cors({
+  origin: (origin, callback) => {
+    const allowedOrigins = [
+      process.env.LOCAL,
+      process.env.ORIGIN,
+    ];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 
 app.use(express.json());
